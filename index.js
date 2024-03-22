cc1cbd0296c720ecc28a1d15803abd7a23185702
Sacre-MBIKU-WAKANE <sacrembiku@mukwakani.com>
2024-03-22 19:58:26 +0100

    define basic routes attached to users ressources

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const bcrypt = require("bcrypt");
+const prisma = require("./database/prisma");
 
 dotenv.config();
 const PORT = process.env.PORT;
 const server = express();
 
+server.use(express.json());
+
 server.get("/", (req, res) => {
   res.send("<h1>Bienvenue au serveur C3</h1>");
+});
+
+server.get("/users", async (req, res) => {
+  try {
+    const users = await prisma.users.findMany();
+    res.status(200).json({ users });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+server.post("/users", async (req, res) => {
+  try {
+    const { firstname, lastname, email } = req.body;
+    let { password } = req.body;
+    password = bcrypt.hashSync(password, 10);
+    const role = "Public";
+    const users = await prisma.users.create({
+      data: { firstname, lastname, password, email, role },
+    });
+    res.status(200).json({ users });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
 
 server.listen(PORT, () => console.log(`Server started on ${PORT}`));
---
+++ prisma/migrations/20240322183947_create/migration.sql
@@ -0,0 +1,32 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('Apprenants', 'Coachs', 'Public');
+
+-- CreateEnum
+CREATE TYPE "Brand" AS ENUM ('HP', 'DELL', 'IMAC');
+
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" SERIAL NOT NULL,
+    "firstname" TEXT NOT NULL,
+    "lastname" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "role" "Role" NOT NULL,
+    "computerId" TEXT,
+
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Computers" (
+    "tag" TEXT NOT NULL,
+    "brand" "Brand" NOT NULL,
+
+    CONSTRAINT "Computers_pkey" PRIMARY KEY ("tag")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
+
+-- AddForeignKey
+ALTER TABLE "Users" ADD CONSTRAINT "Users_computerId_fkey" FOREIGN KEY ("computerId") REFERENCES "Computers"("tag") ON DELETE SET NULL ON UPDATE CASCADE;
